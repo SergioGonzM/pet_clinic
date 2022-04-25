f78b55091fe546df192fe8112b0768bff3131dc4
SergioGonzM <sgonzalezm03@hotmail.com>
2022-04-25 14:59:13 -0500

    Add pet types table

--- priv/repo/migrations/20220423023440_create_pet_types_table.exs
+++ priv/repo/migrations/20220423023440_create_pet_types_table.exs
@@ -1,7 +1,51 @@
 defmodule PetClinic.Repo.Migrations.CreatePetTypesTable do
   use Ecto.Migration
+  import Ecto.Query
+  alias PetClinic.Repo
+  alias PetClinic.PetClinicService.Pet
+  alias PetClinic.PetClinicService.PetType
 
   def change do
+
+    pets = Repo.all(Pet)
+    types = Repo.all(from p in Pet, select: [p.type], distinct: true) |> List.flatten()
+
+    create table("pet_types") do
+      add :name, :string
+      timestamps()
+    end
+    
+    flush()
+
+    Enum.map(pet_types, fn t -> Repo.insert(PetType{name: t})end)
+
+    alter table("pets") do
+      remove :type
+      add :type_id, references("pet_types")
+    end
+
+    flush()
+
+    Enum.map(pets, fn pet -> 
+      %Species{id: sp_id} = Species |> Repo.get_by(name: pet.type)
+      q = "UPDATE pets SET species = $1 WHERE id = $2;"
+      Repo.query!(q, [sp_id, pet.id])
+    end
+
+
+
+    alter table(:pets) do
+      remove :species
+      add :species_id, references(:species)
+    end
+
+    flush()
+
+    Enum.each(pets, fn pet ->
+      %PetType{id: pet_type_id} = Repo.get_by(PetType, name: pet.type)
+      update = "UPDATE pets SET type_id = $1 WHERE id = $2"
+      Repo.query!(update, [pet.species_id, pet.id])
+    end)
 
   end
 end
